897c3dd7e72fa02f198ab4f254ff9968339858d6
belal <belalhesham616@gmail.com>
2026-04-04 17:54:19 -0500

    create theme and layout

---
+++ resources/views/layout/forntend/extend.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.forntend.app')
+@section('title')
+    
+@endsection
+
+@section('body')
+    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('forntend.index');
+});
+Route::get('/contact', function () {
+    return view('forntend.contact-us');
 });
