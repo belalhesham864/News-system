6bb270de397c3b42eb668bc625a14b4cdb7beff2
belal <belalhesham616@gmail.com>
2026-04-04 00:19:00 -0500

    news migration

---
+++ database/migrations/2026_04_04_043558_create_settings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('side_name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('favicon');
+            $table->string('logo');
+            $table->string('facebook');
+            $table->string('tiwter');
+            $table->string('instgram');
+            $table->string('youtube');
+            $table->string('country');
+            $table->string('city');
+            $table->string('street');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+    }
+};
