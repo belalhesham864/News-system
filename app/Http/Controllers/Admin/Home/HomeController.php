b2b0580bf9bf3322dddee13801a04bf7e4b13f11
belal <belalhesham616@gmail.com>
2026-05-06 15:30:02 -0500

    Chart in home page

--- app/Http/Controllers/Admin/Home/HomeController.php
+++ app/Http/Controllers/Admin/Home/HomeController.php
@@ -4,10 +4,56 @@ namespace App\Http\Controllers\Admin\Home;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use LaravelDaily\LaravelCharts\Classes\LaravelChart;
 class HomeController extends Controller
 {
     public function index(){
-        return view('admin.index');
+        $chart_options = [
+    'chart_title' => 'Posts by months',
+    'report_type' => 'group_by_date',
+    'model' => 'App\Models\Post',
+    'group_by_field' => 'created_at',
+    'group_by_period' => 'day',
+    'chart_type' => 'line',
+      'filter_field' => 'created_at',
+    'filter_days' => 30, 
+];
+$chart1 = new LaravelChart($chart_options);
+        $chart_options_users = [
+    'chart_title' => 'Users by Month',
+    'report_type' => 'group_by_date',
+    'model' => 'App\Models\User',
+    'group_by_field' => 'created_at',
+    'group_by_period' => 'month',
+    'chart_type' => 'bar',
+      'filter_field' => 'created_at',
+    'filter_days' => 360, 
+];
+$chart_users = new LaravelChart($chart_options_users);
+        $chart_comment = [
+    'chart_title' => 'comment by Month',
+    'report_type' => 'group_by_date',
+    'model' => 'App\Models\Comment',
+    'group_by_field' => 'created_at',
+    'group_by_period' => 'month',
+    'chart_type' => 'pie',
+      'filter_field' => 'created_at',
+    'filter_days' => 360, 
+];
+$chart_comment = new LaravelChart($chart_comment);
+        $chart_contatct = [
+    'chart_title' => 'Contatct by Month',
+    'report_type' => 'group_by_date',
+    'model' => 'App\Models\Contact',
+    'group_by_field' => 'created_at',
+    'group_by_period' => 'day',
+    'chart_type' => 'line',
+      'filter_field' => 'created_at',
+    'filter_days' => 30, 
+];
+$chart_contatct = new LaravelChart($chart_contatct);
+
+
+        return view('admin.index', compact('chart1','chart_users','chart_comment','chart_contatct'));
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -19,78 +19,39 @@ home
 <!--Charts-->
                     <div class="row">
 
-                        <!-- Area Chart -->
-                        <div class="col-xl-8 col-lg-7">
-                            <div class="card shadow mb-4">
-                                <!-- Card Header - Dropdown -->
-                                <div
-                                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                    <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
-                                    <div class="dropdown no-arrow">
-                                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                                        </a>
-                                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                                            aria-labelledby="dropdownMenuLink">
-                                            <div class="dropdown-header">Dropdown Header:</div>
-                                            <a class="dropdown-item" href="#">Action</a>
-                                            <a class="dropdown-item" href="#">Another action</a>
-                                            <div class="dropdown-divider"></div>
-                                            <a class="dropdown-item" href="#">Something else here</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- Card Body -->
-                                <div class="card-body">
-                                    <div class="chart-area">
-                                        <canvas id="myAreaChart"></canvas>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                    <div class="card-body shadow col-6">
+
+                    <h4>{{ $chart1->options['chart_title'] }}</h4>
+                    {!! $chart1->renderHtml() !!}
+
+                </div>
+                    <div class="card-body shadow col-6">
+
+                    <h4>{{ $chart_users->options['chart_title'] }}</h4>
+                    {!! $chart_users->renderHtml() !!}
+
+                </div>
 
                         <!-- Pie Chart -->
-                        <div class="col-xl-4 col-lg-5">
-                            <div class="card shadow mb-4">
-                                <!-- Card Header - Dropdown -->
-                                <div
-                                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                    <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
-                                    <div class="dropdown no-arrow">
-                                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                                        </a>
-                                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                                            aria-labelledby="dropdownMenuLink">
-                                            <div class="dropdown-header">Dropdown Header:</div>
-                                            <a class="dropdown-item" href="#">Action</a>
-                                            <a class="dropdown-item" href="#">Another action</a>
-                                            <div class="dropdown-divider"></div>
-                                            <a class="dropdown-item" href="#">Something else here</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- Card Body -->
-                                <div class="card-body">
-                                    <div class="chart-pie pt-4 pb-2">
-                                        <canvas id="myPieChart"></canvas>
-                                    </div>
-                                    <div class="mt-4 text-center small">
-                                        <span class="mr-2">
-                                            <i class="fas fa-circle text-primary"></i> Direct
-                                        </span>
-                                        <span class="mr-2">
-                                            <i class="fas fa-circle text-success"></i> Social
-                                        </span>
-                                        <span class="mr-2">
-                                            <i class="fas fa-circle text-info"></i> Referral
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                  
+                    </div>
+                    <div class="row">
+
+                    <div class="card-body shadow col-6">
+
+                    <h4>{{ $chart_comment->options['chart_title'] }}</h4>
+                    {!! $chart_comment->renderHtml() !!}
+
+                </div>
+                    <div class="card-body shadow col-6">
+
+                    <h4>{{ $chart_contatct->options['chart_title'] }}</h4>
+                    {!! $chart_contatct->renderHtml() !!}
+
+                </div>
+
+                        <!-- Pie Chart -->
+                  
                     </div>
 
                     <!-- posts && comment Row -->
@@ -98,3 +59,13 @@ home
 
                 </div>
 @endsection
+@push('js')
+
+{!! $chart1->renderChartJsLibrary() !!}
+{!! $chart1->renderJs() !!}
+
+{!! $chart_users->renderJs() !!}
+{!! $chart_comment->renderJs() !!}
+{!! $chart_contatct->renderJs() !!}
+
+@endpush
